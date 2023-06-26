50de23b0841a61196a8a4801d3274dff4b1b3f1f
Thiveeya <rbsthivi97@gmail.com>
2023-06-26 20:56:08 +0530

    UserProfileTASK

---
+++ src/Data/data.js
@@ -0,0 +1,44 @@
+export const data = [
+    {
+      id: "100",
+      name: "Thiveeya",
+      batch: "B47WD",
+      email: "rbsthivi@gmail.com",
+      experience: 1,
+    },
+    {
+      id: "101",
+      name: "Krithi",
+      batch: "B47WD",
+      email: "krithi@gmail.com",
+      experience: 2,
+    },
+    {
+      id: "102",
+      name: "Suguna",
+      batch: "B47WD",
+      email: "suguna@gmail.com",
+      experience: 3,
+    },
+    {
+      id: "103",
+      name: "Sriram",
+      batch: "B47WD",
+      email: "sriram@gmail.com",
+      experience: 3,
+    },
+    {
+      id: "104",
+      name: "Kowsi",
+      batch: "B47WD",
+      email: "kow@gmail.com",
+      experience: 2,
+    },
+    {
+      id: "105",
+      name: "Devi",
+      batch: "B47WD",
+      email: "dev@gmail.com",
+      experience: 1,
+    },
+  ];
