28e557a26b36afb3832fbbb3bacc3d9caea22c58
Thiveeya <rbsthivi97@gmail.com>
2023-07-10 21:34:32 +0530

    change4

--- src/Components/NoPage.js
+++ src/Components/NoPage.js
@@ -4,7 +4,7 @@ export default function NoPage(){
     return(
         <div className="no-page">
             <h3>Error Back to Home page</h3>
-            <a href="/">back to safely</a>
+             <a href="/!">back to safely</a>
         </div>
     )
 }
