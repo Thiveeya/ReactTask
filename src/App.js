6e76eadc6964bcf4ac2fe61ad6d6af79f25cf84e
Thiveeya <rbsthivi97@gmail.com>
2023-07-23 01:28:46 +0530

    task42-fe

--- src/App.js
+++ src/App.js
@@ -1,118 +1,67 @@
-import "./App.css";
-import { Route, Switch } from "react-router-dom";
-import { Library } from "./Library";
-import { Home } from "./Home";
-
-import { Economics } from "./Economics";
-import { History } from "./History";
-import { Science } from "./Science";
-import { Geography } from "./Geography";
-
-import { useState } from "react";
-
-import { historydata } from "./historydata";
-import { scidata } from "./scidata";
-import { geodata } from "./geodata";
-import { ecodata } from "./ecodata";
-
-import { Scienceview } from "./Scienceview";
-import { Hisview } from "./Hisview";
-import { Geoview } from "./Geoview";
-import { Ecoview } from "./Ecoview";
-
-import { Ecoadd } from "./Ecoadd";
-import { Geoadd } from "./Geoadd";
-import { Hisadd } from "./Hisadd";
-import { Scienceadd } from "./Scienceadd";
-
-import { Hisedit } from "./Hisedit";
-import { Ecoedit } from "./Ecoedit";
-import { Geoedit } from "./Geoedit";
-import { Scienceedit } from "./Scienceedit";
-
+import { useEffect, useState } from "react";
+import Todo from "./components/Todo";
+import { addTodo, deleteTodo, getAllTodo, updateTodo } from "./api/HandleApi";
 
 function App() {
-  
-  
-  const[his,setHis]=useState(historydata);
-  const[science,setScience]=useState(scidata);
-  const[geo,setGeo]=useState(geodata);
-  const[eco,setEco]=useState(ecodata);
+
+  const [todo, setTodo] = useState([]);
+  const [text, setText] = useState("");
+  const [isUpdating, setIsUpdating] = useState(false)
+  const [todoId, setTodoId] = useState("");
+  const [check, setCheck] = useState(false)
+
+  useEffect(() => {
+    getAllTodo(setTodo)
+  }, [])
+
+  const UpdateTodo = (_id, text) => {
+    if (text) {
+      setIsUpdating(true)
+      setText(text)
+      setTodoId(_id)
+      setCheck(false)
+    } else {
+      setCheck(true)
+    }
+  }
 
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <Home/>
-          </Route>
-        <Route path="/library">
-          <Library />
-        </Route>
-        
-        <Route path="/history">
-          <History his={his} setHistory={setHis}/>
-        </Route>
-        <Route path="/science">
-          <Science science={science} setScience={setScience} />
-        </Route>
-        <Route path="/geography">
-          <Geography geo={geo} setGeo={setGeo}/>
-        </Route>
-        <Route path="/economics">
-          <Economics eco={eco} setEco={setEco}/>
-        </Route>
+      <div className="container">
+        <h1>ToDo App</h1>
+        <div className="top">
+          <form>
+          <input type="text"
+            placeholder="Add ToDos..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            required
+          />
+          <button type="submit" className="add" onClick={isUpdating ?
+            (e) => {
+              e.preventDefault();
+              updateTodo(todoId, text, setTodo, setText, setIsUpdating,setCheck)} :
+            (e) =>{
+              e.preventDefault();
+              addTodo(text, setText, setTodo,setCheck)}}
+          >
+            {isUpdating ? "Update" : "Add"}
+          </button>
+          </form>
+          </div>
+          <div>
+            {!text && check && <span className="validate">Required</span>}
+          </div>
+          <div className="list">
+            {todo.map(item => <Todo key={item._id} text={item.text} updateTodo={() => UpdateTodo(item._id, item.text)}
+              deleteTodo={() => deleteTodo(item._id, setTodo)}
+            />
+            )}
 
-
-
-        <Route path="/ecoedit/:id">
-          <Ecoedit eco={eco} setEco={setEco}/>
-        </Route>
-        <Route path="/ecoview/:id">
-          <Ecoview eco={eco} setEco={setEco}/>
-        </Route>
-        <Route path="/ecoadd">
-          <Ecoadd eco={eco} setEco={setEco}/>
-        </Route>
-
-
-        <Route path="/geoadd">
-        <Geoadd geo={geo} setGeo={setGeo}/>
-        </Route>
-        <Route path="/geoedit/:id">
-        <Geoedit geo={geo} setGeo={setGeo}/>
-        </Route>
-        <Route path="/geoview/:id">
-        <Geoview geo={geo} setGeo={setGeo}/>
-        </Route>
-
-
-        <Route path="/hisview/:id">
-        <Hisview his={his} setHis={setHis}/>
-        </Route>
-        <Route path="/hisedit/:id">
-        <Hisedit his={his} setHis={setHis}/>
-        </Route>
-        <Route path="/hisadd">
-        <Hisadd his={his} setHis={setHis}/>
-        </Route>
-
-
-        <Route path="/scienceadd">
-        <Scienceadd science={science} setScience={setScience} />
-        </Route>
-        <Route path="/scienceedit/:id">
-        <Scienceedit science={science} setScience={setScience} />
-        </Route>
-        <Route path="/scienceview/:id">
-        <Scienceview science={science} setScience={setScience} />
-        </Route>
-
-      </Switch>
-    </div>
-  );
+          </div>
+        </div>
+      </div>
+      );
 }
 
-export default App;
-
-
-
+      export default App;
---
+++ src/api/HandleApi.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+
+const Url = "https://todo-be-2e1b.onrender.com";
+
+const getAllTodo = (setTodo) => {
+    axios.get(Url)
+        .then(({ data }) => {
+            setTodo(data);
+        })
+}
+
+const addTodo = (text, setText, setTodo, setCheck) => {
+    if (text) {
+        setCheck(false)
+        axios.post(`${Url}/save`, { text })
+            .then(data => {
+                setText("")
+                getAllTodo(setTodo)
+            })
+            .catch(err => console.error(err));
+    }else{
+        setCheck(true)
+    }
+    
+}
+
+const updateTodo = (todoId, text, setTodo, setText, setIsUpdating,setCheck) => {
+    if (text) {
+        setCheck(false)
+        axios.post(`${Url}/update`, { _id: todoId, text })
+            .then(data => {
+                setText("")
+                setIsUpdating(false)
+                getAllTodo(setTodo)
+            })
+            .catch(err => console.error(err));
+    }else{
+        setCheck(true)
+    }
+
+}
+
+const deleteTodo = (_id, setTodo) => {
+
+    axios.post(`${Url}/delete`, { _id })
+        .then(data => {
+            getAllTodo(setTodo)
+        })
+        .catch(err => console.error(err));
+}
+
+export { getAllTodo, addTodo, updateTodo, deleteTodo }
