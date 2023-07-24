fbcab8133cd9d764fdbe6031d185a5fb5edb36ff
Thiveeya <rbsthivi97@gmail.com>
2023-07-24 22:40:48 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,67 +1,24 @@
-import { useEffect, useState } from "react";
-import Todo from "./components/Todo";
-import { addTodo, deleteTodo, getAllTodo, updateTodo } from "./api/HandleApi";
+import './App.css';
+import Login from './Components/Login';
+import PasswordReset from './Components/PasswordReset';
+import SignUp from './Components/SignUP';
+import { Route,Routes} from 'react-router-dom'
+import LoginSuccess from './Components/LoginSuccess';
+
 
 function App() {
 
-  const [todo, setTodo] = useState([]);
-  const [text, setText] = useState("");
-  const [isUpdating, setIsUpdating] = useState(false)
-  const [todoId, setTodoId] = useState("");
-  const [check, setCheck] = useState(false)
-
-  useEffect(() => {
-    getAllTodo(setTodo)
-  }, [])
-
-  const UpdateTodo = (_id, text) => {
-    if (text) {
-      setIsUpdating(true)
-      setText(text)
-      setTodoId(_id)
-      setCheck(false)
-    } else {
-      setCheck(true)
-    }
-  }
 
   return (
     <div className="App">
-      <div className="container">
-        <h1>ToDo App</h1>
-        <div className="top">
-          <form>
-          <input type="text"
-            placeholder="Add ToDos..."
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            required
-          />
-          <button type="submit" className="add" onClick={isUpdating ?
-            (e) => {
-              e.preventDefault();
-              updateTodo(todoId, text, setTodo, setText, setIsUpdating,setCheck)} :
-            (e) =>{
-              e.preventDefault();
-              addTodo(text, setText, setTodo,setCheck)}}
-          >
-            {isUpdating ? "Update" : "Add"}
-          </button>
-          </form>
-          </div>
-          <div>
-            {!text && check && <span className="validate">Required</span>}
-          </div>
-          <div className="list">
-            {todo.map(item => <Todo key={item._id} text={item.text} updateTodo={() => UpdateTodo(item._id, item.text)}
-              deleteTodo={() => deleteTodo(item._id, setTodo)}
-            />
-            )}
-
-          </div>
-        </div>
-      </div>
-      );
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/login/success" element={<LoginSuccess />} />
+        <Route path="/password-reset" element={<PasswordReset />} />
+      </Routes>
+    </div>
+  );
 }
 
-      export default App;
+export default App;
---
+++ src/Components/LoginSuccess/index.js
@@ -0,0 +1,10 @@
+const LoginSuccess = (props) => {
+    return (
+        <div className="text" >
+            <h1>Welcome to our APP</h1>
+            <alert>Your Successfully logged in</alert>
+        </div>
+    )
+}
+
+export default LoginSuccess;
